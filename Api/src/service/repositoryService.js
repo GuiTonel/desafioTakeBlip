d468402f7821d959c006450ab8dea8fa32bbe4e1
GuiTonel <psadatv@gmail.com>
2021-05-28 12:54:11 -0300

    fix bugs

--- Api/src/service/repositoryService.js
+++ Api/src/service/repositoryService.js
@@ -1,6 +1,6 @@
-const repositoryRequest = require( '../request/repositoryRequest' )
-const RepositoryDTO = require( '../request/DTO/repositoryDTO' )
-const RepositoryModel = require( '../model/repositoryModel' )
+const repositoryRequest = require('../request/repositoryRequest')
+const RepositoryDTO = require('../request/DTO/repositoryDTO')
+const RepositoryModel = require('../model/repositoryModel')
 const InternalException = require('../exception/internalException')
 const TimeoutException = require('../exception/timeoutException')
 
@@ -10,46 +10,51 @@ class RepositoryService {
 
     TIMEOUT = 5000
 
-    async getRepositoriesTakeBlip( sort, qtd = 20, language, order, page = 1 ) {
+    async getRepositoriesTakeBlip(sort, qtd = 20, language, order, page = 1) {
         var groupedRepositoriesResponseJson = {}
-        var repositoryDTO = new RepositoryDTO( { sort: sort, direction: order, per_page: qtd, page: page } )
+        var repositoryDTO = new RepositoryDTO({
+            sort: sort,
+            direction: order,
+            per_page: qtd,
+            page: page
+        })
         var repositoryIndex = 0
         let startTime = Date.now();
 
         try {
             do {
-                if ( Date.now() - startTime >= this.TIMEOUT ) {
+                if (Date.now() - startTime >= this.TIMEOUT) {
                     throw new TimeoutException()
                 }
-                let responseRepository = await repositoryRequest.getTakeBlipRepositories( repositoryDTO )
- 
-                for ( let repository of responseRepository ) {
-                    if ( String( repository.language ).toUpperCase() !== language.toUpperCase() && ( typeof language !== 'undefined' ) ) continue
-                    
-                    let repositoryModel = new RepositoryModel( repository.full_name, repository.description, repository.language )
-                    
+                let responseRepository = await repositoryRequest.getTakeBlipRepositories(repositoryDTO)
+
+                for (let repository of responseRepository) {
+                    if (String(repository.language).toUpperCase() !== language.toUpperCase() && (typeof language !== 'undefined')) continue
+
+                    let repositoryModel = new RepositoryModel(repository.full_name, repository.description, repository.language)
+
                     repositoryIndex += 1
-                    groupedRepositoriesResponseJson[ `repository${ repositoryIndex }` ] = repositoryModel     
+                    groupedRepositoriesResponseJson[`repository${ repositoryIndex }`] = repositoryModel
+
+                    if (repositoryIndex == qtd) break
                 }
-                
+
                 repositoryDTO.params.page += 1
-                repositoryDTO.params.per_page = qtd - repositoryIndex
-    
-            } while ( repositoryIndex < qtd )
 
-            return groupedRepositoriesResponseJson 
+            } while (repositoryIndex < qtd)
 
-        } catch ( err ) {
-            console.log( err )
-            if ( err instanceof TimeoutException ) {
+            return groupedRepositoriesResponseJson
+
+        } catch (err) {
+            console.log(err)
+            if (err instanceof TimeoutException) {
                 throw new TimeoutException()
-            } 
-            else {
+            } else {
                 throw new InternalException()
             }
         }
     }
 
-}   
+}
 
 module.exports = new RepositoryService()
